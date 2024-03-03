7e9eec3ba4481295889508e08f89fe10f3221948
manhhungking <trinhmanhhung03@gmail.com>
2024-03-03 21:00:46 +0200

    Update client.py

--- client.py
+++ client.py
@@ -13,23 +13,28 @@ pygame.display.set_caption("Client")
 
 
 class Button:
-    def __init__(self, text, x, y, colour):
+
+    def __init__(self, text, x, y, width, height, colour):
         self.text = text
-        self.x = x
-        self.y = y
+        self.width = width
+        self.height = height
         self.colour = colour
-        self.width = 150
-        self.height = 100
+
+        # Adjust x and y to center the button
+        self.x = x - self.width // 2
+        self.y = y - self.height // 2
 
     def draw(self, win):
         pygame.draw.rect(win, self.colour, (self.x, self.y, self.width, self.height))
-        font = pygame.font.SysFont("verdana", 30)
+        font = pygame.font.SysFont("verdana", 20)
         text = font.render(self.text, 1, (255, 255, 255))
+        text_width, text_height = text.get_size()
+
         win.blit(
             text,
             (
-                self.x + round(self.width / 2) - round(text.get_width() / 2),
-                self.y + round(self.height / 2) - round(text.get_height() / 2),
+                self.x + round(self.width / 2) - round(text_width / 2),
+                self.y + round(self.height / 2) - round(text_height / 2),
             ),
         )
 
@@ -40,6 +45,56 @@ class Button:
             return True
         else:
             return False
+
+
+class InputBox:
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color_inactive = pygame.Color("lightskyblue3")
+        self.color_active = pygame.Color("dodgerblue2")
+        self.color = self.color_inactive
+        self.text = ""
+        self.font = pygame.font.SysFont("verdana", 30)
+        self.active = False
+        self.text_surface = self.font.render(self.text, True, self.color_active)
+
+    def handle_event(self, event):
+        if event == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.active = not self.active
+            else:
+                self.active = False
+            self.color = self.color_active if self.active else self.color_inactive
+        if event == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    # Send the entered ID to the server
+                    network.send(f"id:{self.text}")
+                    self.text = ""
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                self.text_surface = self.font.render(self.text, True, self.color_active)
+
+    def update(self):
+        width = max(200, self.text_surface.get_width() + 10)
+        self.rect.w = width
+
+    def draw(self, win):
+        width = max(200, self.text_surface.get_width() + 10)
+        pygame.draw.rect(win, self.color, self.rect, 2)
+        pygame.draw.rect(
+            win,
+            (255, 255, 255),
+            (self.rect.x + 5, self.rect.y + 5, width - 10, self.rect.height - 10),
+        )
+        win.blit(
+            self.text_surface,
+            (self.rect.x + 10, self.rect.y + 10, width - 20, self.rect.height - 20),
+        )
+        pygame.display.flip()
 
 
 # functions
@@ -85,8 +140,6 @@ def re_draw_window(win, game, player):
             else:
                 text2 = font.render("Waiting...", 1, (0, 102, 102))
 
-            # render text on screen
-
         if player == 1:
             win.blit(text2, (100, 350))
             win.blit(text1, (400, 350))
@@ -100,11 +153,17 @@ def re_draw_window(win, game, player):
     pygame.display.update()
 
 
+enter_id_button = Button(
+    "Enter the ID of the player", WIDTH // 2, 450, 300, 50, (102, 0, 51)
+)
+
 buttons = [
-    Button("Rock", 50, 500, (51, 102, 0)),
-    Button("Scissors", 250, 500, (0, 51, 102)),
-    Button("Paper", 450, 500, (102, 0, 51)),
+    Button("Rock", WIDTH // 4, 500, 150, 100, (51, 102, 0)),
+    Button("Scissors", WIDTH // 2, 500, 150, 100, (0, 51, 102)),
+    Button("Paper", 3 * WIDTH // 4, 500, 150, 100, (102, 0, 51)),
 ]
+
+input_box = InputBox(WIDTH // 2 - 150, 450, 300, 50)
 
 
 # functions
@@ -112,73 +171,122 @@ def introduction_screen():
     run = True
     clock = pygame.time.Clock()
 
+    intro_text1 = "Welcome to Rock-Paper-Scissors Online!"
+    intro_text2 = "Gameplay: Each player has a maximum of 3 points,"
+    intro_text3 = "and the game runs for a maximum of 5 rounds."
+    intro_text4 = "The first player to reach 3 points wins the game."
+
     while run:
         clock.tick(60)
         window.fill((144, 199, 255))
-        font = pygame.font.SysFont("verdana", 40)
-        text = font.render("Welcome to Rock-Paper-Scissors Game!", 1, (0, 5, 10))
-        window.blit(
-            text, (WIDTH / 2 - text.get_width() / 2, HEIGHT / 2 - text.get_height() / 2)
+        font = pygame.font.SysFont("verdana", 20)
+
+        text1 = font.render(intro_text1, 1, (0, 5, 10))
+        text2 = font.render(intro_text2, 1, (0, 5, 10))
+        text3 = font.render(intro_text3, 1, (0, 5, 10))
+        text4 = font.render(intro_text4, 1, (0, 5, 10))
+
+        window.blit(text1, (WIDTH / 2 - text1.get_width() / 2, 50))
+        window.blit(text2, (WIDTH / 2 - text2.get_width() / 2, 100))
+        window.blit(text3, (WIDTH / 2 - text3.get_width() / 2, 150))
+        window.blit(text4, (WIDTH / 2 - text4.get_width() / 2, 200))
+
+        # Display buttons for options
+        button_font = pygame.font.SysFont("verdana", 24)
+
+        play_random_button = Button(
+            "Play with a random player", WIDTH / 2, 300, 300, 50, (51, 102, 0)
         )
+        play_random_button.draw(window)
+
+        enter_id_button = Button(
+            "Enter the ID of the player", WIDTH / 2, 450, 300, 50, (102, 0, 51)
+        )
+        enter_id_button.draw(window)
+
         pygame.display.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 run = False
+                return "quit"
             if event.type == pygame.MOUSEBUTTONUP:
-                run = False
+                position = pygame.mouse.get_pos()
+                if play_random_button.click(position):
+                    return "random"
+                elif enter_id_button.click(position):
+                    return "id"
 
 
-def menu_screen():
+def menu_screen(network):
     run = True
     clock = pygame.time.Clock()
 
+    input_box = InputBox(
+        WIDTH // 2 - 150, 500, 300, 40
+    )  # Adjust position and size as needed
+
+    entering_id = False  # Flag to indicate if entering ID mode
+
     while run:
         clock.tick(60)
-        window.fill((144, 199, 255))
-        font = pygame.font.SysFont("verdana", 40)
-        text = font.render("Choose an option:", 1, (0, 5, 10))
-        window.blit(
-            text, (WIDTH / 2 - text.get_width() / 2, HEIGHT / 4 - text.get_height() / 2)
-        )
-
-        button_play_random = Button("Play with Random Player", 150, 300, (51, 102, 0))
-        button_play_id = Button("Play with Specific ID", 150, 450, (0, 51, 102))
-
-        button_play_random.draw(window)
-        button_play_id.draw(window)
-
-        pygame.display.update()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 run = False
             if event.type == pygame.MOUSEBUTTONUP:
                 position = pygame.mouse.get_pos()
-                if button_play_random.click(position):
+                if enter_id_button.click(position):
+                    entering_id = True
+                elif entering_id and input_box.rect.collidepoint(position):
+                    entering_id = True
+                else:
                     run = False
-                    play_game_with_id = False
-                elif button_play_id.click(position):
-                    run = False
-                    play_game_with_id = True
 
-    if play_game_with_id:
-        player_id = input("Enter the ID of the player you want to play with: ")
-    else:
-        player_id = None
+            # Handle events for the input box only when entering_id is True
+            if entering_id:
+                input_box.handle_event(event)
 
-    network = Network(player_id)
-    player = int(network.get_player())
-    print("You are player number: {}".format(player))
-    introduction_screen()
+        window.fill((144, 199, 255))
 
-    main(network, player)
+        font = pygame.font.SysFont("verdana", 60)
+        text = font.render("Click to Play!", 1, (0, 5, 10))
+        window.blit(
+            text, (WIDTH / 2 - text.get_width() / 2, HEIGHT / 2 - text.get_height() / 2)
+        )
+
+        pygame.display.update()
+
+    player_choice = introduction_screen()
+
+    if player_choice == "quit":
+        pygame.quit()
+    elif player_choice == "random":
+        # Connect to the server and send the player choice
+        network.connect()
+        network.send("random")
+
+        # Start the game
+        main(network)
+    elif player_choice == "id":
+        # Get the ID from the input box only if entering_id is True
+        player_id = input_box.text if entering_id else ""
+
+        # Connect to the server and send the player choice
+        network.connect()
+        network.send(f"id:{player_id}")
+
+        # Start the game
+        main(network)
 
 
-def main(network, player):
+# main function
+def main(network):
     run = True
     clock = pygame.time.Clock()
+    player = int(network.get_player())
+    print("You are player number: {}".format(player))
 
     while run:
         clock.tick(60)
@@ -239,4 +347,5 @@ def main(network, player):
 
 
 while True:
-    menu_screen()
+    network = Network()
+    menu_screen(network)
